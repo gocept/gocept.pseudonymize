39a90dbf381380ca2db7e9a5b45aad4861baf998
Michael Howitz <mh@gocept.com>
2015-10-02 13:00:44 +0200

    Officially support Python 3.4 and 3.5.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 gocept gmbh & co. kg
+# Copyright (c) 2013-2015 gocept gmbh & co. kg
 # See also LICENSE.txt
 
 # This should be only one line. If it must be multi-line, indent the second
@@ -52,6 +52,8 @@ Programming Language :: Python :: 2
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
 """[:-1].split('\n'),
     description=__doc__.strip(),
     long_description='\n\n'.join([
@@ -59,7 +61,7 @@ Programming Language :: Python :: 3.3
         read('README.txt'),
         read('HACKING.txt'),
         read('CHANGES.txt'),
-        ]),
+    ]),
     namespace_packages=['gocept'],
     packages=find_packages('src'),
     package_dir={'': 'src'},
